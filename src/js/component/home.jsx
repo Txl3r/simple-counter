02bc26e27ac94da347dd75a0050c2460bf4845c7
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-03-29 20:58:00 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,9 +2,13 @@ import Card from "./card.jsx";
 import React from "react";
 //create your first component
 const Home = () => {
+	let word = {
+		marginLeft: "40px",
+		fontSize: "50px",
+	};
 	return (
 		<div className="card bg-dark text-light" style={{ width: "18rem" }}>
-			<div className="card-body fa fa-clock text-light">
+			<div className="card-body fa fa-clock text-light" style={word}>
 				<Card number={1000} />
 				<Card number={100} />
 				<Card number={10} />
